1eb3378bf1ea40e8b5741c353b7ec5b333d5ae9b
Mark Standen <mark@markstanden.dev>
2022-02-15 18:01:11 +0000

    first two methods

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -1,12 +1,18 @@
 using System;
 
-public static class LogAnalysis 
+public static class LogAnalysis
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string str, string phrase)
+    {
+        int startIndex = str.IndexOf("phrase") + phrase.Length;
+        return str.Substring(startIndex, str.Length - startIndex);
+    }
 
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
-    // TODO: define the 'Message()' extension method on the `string` type
+    public static string SubstringBetween(this string str, string startPhrase, string endPhrase)
+    {
+        string start = str.SubstringAfter(startPhrase);
+        int end = start.IndexOf(endPhrase);
 
-    // TODO: define the 'LogLevel()' extension method on the `string` type
+        return start.Substring(0, end);
+    }
 }
