4ca7aea748ed0080b7e433b4a51cb8d0bd1df3e2
Mark Standen <mark@markstanden.dev>
2022-02-18 12:12:39 +0000

    updated formatting

--- dominoes/Dominoes.cs
+++ dominoes/Dominoes.cs
@@ -34,13 +34,13 @@ public static class Dominoes
                         .Any(args => GetOppositeAndRemainder(args.chain, args.withoutDom));
     }
 
-/// <summary>
-/// Returns an updated IEnumerable without the first occurrence of the item.
-/// </summary>
-/// <param name="sequence">The sequence to remove the item from</param>
-/// <param name="item">The item to be excluded from the sequence</param>
-/// <typeparam name="T">The Type of the items in the sequence</typeparam>
-/// <returns>A new, ordered sequence that doesn't contain the first occurrence of item</returns>
+    /// <summary>
+    /// Returns an updated IEnumerable without the first occurrence of the item.
+    /// </summary>
+    /// <param name="sequence">The sequence to remove the item from</param>
+    /// <param name="item">The item to be excluded from the sequence</param>
+    /// <typeparam name="T">The Type of the items in the sequence</typeparam>
+    /// <returns>A new, ordered sequence that doesn't contain the first occurrence of item</returns>
     private static IEnumerable<T> ReturnWithout<T>(this IEnumerable<T> sequence, T item)
     {
         var copy = sequence.ToList();
