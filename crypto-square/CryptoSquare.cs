22d43521d8dd2eabeb191bfb9ac543ba127fec8b
Mark Standen <mark@markstanden.dev>
2022-02-19 05:48:57 +0000

    working solution to the cryptographic square challenge

--- crypto-square/CryptoSquare.cs
+++ crypto-square/CryptoSquare.cs
@@ -1,56 +1,87 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class CryptoSquare
 {
     public static string NormalizedPlaintext(string plaintext)
-        => plaintext.ToLower()
-                    .Replace(" ", "");
+        => Regex.Replace(plaintext, @"([^a-zA-Z0-9]+)", @"")
+                .ToLower();
+
 
 
     public static IEnumerable<string> PlaintextSegments(string plaintext)
     {
-        int chunkSize = CalcColSize(plaintext.Length);
-        return Enumerable.Range(0, plaintext.Length / chunkSize)
-                         .Select(i => plaintext.Substring(i * chunkSize, chunkSize));
+        var normalized = NormalizedPlaintext(plaintext);
+
+        int columns = CalcColSize(normalized.Length);
+        int rows = IntCeiling(normalized.Length, columns);
+
+        string padded = normalized.PadRight(rows * columns);
+
+        return Enumerable.Range(0, rows)
+                         .Select(i => padded.Substring(i * columns, columns));
     }
 
 
+    /// <summary>
+    /// Cleanly divides two integers and
+    /// returns the 'rounded up' ceiling value.
+    /// Takes two integers and returns and int.
+    ///
+    /// How many boxes are needed for x (nominator) items when each box stores y (denominator) items?
+    /// IntCeiling(x, y)
+    ///
+    /// How many boxes are needed for 10 items when each box stores 3 (denominator) items?
+    /// IntCeiling(10, 3) = 4
+    ///
+    /// Removes the need to cast doubles when working with Math.Ceiling.
+    ///
+    /// </summary>
+    /// <param name="nominator">The number to divide.</param>
+    /// <param name="denominator">The number to divide by.</param>
+    /// <returns>The rounded up integer value</returns>
+    private static int IntCeiling(int nominator, int denominator)
+        => (nominator + denominator - 1) / denominator;
+
     private static int CalcColSize(int messageLength)
         => Enumerable.Range(1, messageLength)
-                     .Select(cols => Enumerable.Range(1, messageLength)
-                                               .Where(rows => cols * rows >= messageLength)
-                                               .Where(rows => rows > cols)
-                                               .Where(rows => rows - cols <= 1))
+                     .Select(rows => Enumerable.Range(1, messageLength)
+                                               .Where(cols => cols * rows >= messageLength)
+                                               .Where(cols => cols >= rows)
+                                               .Where(cols => cols - rows <= 1))
                      .SelectMany(x => x)
                      .Min();
 
 
     public static string Encoded(string plaintext)
-    {
-        return PlaintextSegments(plaintext)
-           .Select(str => str.ToArray())
-           .Translate().Select(row => row.ToString()).ToString();
-    }
+        => string.Join(" ", PlaintextSegments(plaintext)
+                          .Select(str => str.ToCharArray())
+                          .Transpose()
+                          .Select(row => string.Join("", row.ToArray())));
 
 
-    private static IEnumerable<IEnumerable<T>> Translate<T>(this IEnumerable<IEnumerable<T>> gridEnumerable)
+    /// <summary>
+    /// Transposes a nested IEnumerable grid
+    /// </summary>
+    /// <param name="gridEnumerable">The nested IEnumerable grid to transpose</param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns>A new Transposed nested IEnumerable grid</returns>
+    private static IEnumerable<IEnumerable<T>> Transpose<T>(this IEnumerable<IEnumerable<T>> gridEnumerable)
     {
         var grid = gridEnumerable.Select(row => row.ToArray())
                                  .ToArray();
-        return Enumerable.Range(0, grid.GetLength(1))
+        return Enumerable.Range(0, grid[0]
+                                   .Length)
                          .Select(col => grid.GetColumn(col));
     }
 
 
-    private static IEnumerable<T> GetColumn<T>(this IReadOnlyList<T[]> grid,  int colNumber)
+    private static IEnumerable<T> GetColumn<T>(this IReadOnlyList<T[]> grid, int colNumber)
         => Enumerable.Range(0, grid.Count)
                      .Select(row => grid[row][colNumber]);
 
 
     public static string Ciphertext(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => plaintext.Length == 0 ? "" : Encoded(plaintext);
 }
