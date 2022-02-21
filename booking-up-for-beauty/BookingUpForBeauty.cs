862e239711720986121aa71d9234dab02e29f52e
Mark Standen <mark@markstanden.dev>
2022-02-21 05:52:42 +0000

    swapped returns for lambda arrows

--- booking-up-for-beauty/BookingUpForBeauty.cs
+++ booking-up-for-beauty/BookingUpForBeauty.cs
@@ -3,31 +3,21 @@ using System;
 static class Appointment
 {
     public static DateTime Schedule(string appointmentDateDescription)
-    {
-        return DateTime.Parse(appointmentDateDescription);
-    }
+        => DateTime.Parse(appointmentDateDescription);
 
 
     public static bool HasPassed(DateTime appointmentDate)
-    {
-        return appointmentDate.CompareTo(DateTime.Now) < 0;
-    }
+        => appointmentDate.CompareTo(DateTime.Now) < 0;
 
 
     public static bool IsAfternoonAppointment(DateTime appointmentDate)
-    {
-        return appointmentDate.Hour is >= 12 and < 18;
-    }
+        => appointmentDate.Hour is >= 12 and < 18;
 
 
     public static string Description(DateTime appointmentDate)
-    {
-        return $"You have an appointment on {appointmentDate.ToShortDateString()} {appointmentDate.ToLongTimeString()}.";
-    }
+        => $"You have an appointment on {appointmentDate.ToShortDateString()} {appointmentDate.ToLongTimeString()}.";
 
 
     public static DateTime AnniversaryDate()
-    {
-        return new DateTime(DateTime.Now.Year, 9, 15);
-    }
+        => new DateTime(DateTime.Now.Year, 9, 15);
 }
