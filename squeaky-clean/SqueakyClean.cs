8ee94ffa1330e66892cbed4642b3898a1eab0656
Mark Standen <mark@markstanden.dev>
2022-02-25 05:14:23 +0000

    completed string manipulations using only regex ;)

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 public static class Identifier
 {
     public static string Clean(string identifier)
-    {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
-    }
+        => identifier.RemoveControl()
+                     .RemoveGreek()
+                     .kebabToCamel()
+                     .RemoveNonLetters()
+                     .RemoveWhiteSpace();
+
+
+    private static string RemoveNonLetters(this string phrase)
+        => phrase.ReplaceWith(@"[^\p{L}\s]", "");
+
+
+    private static string RemoveControl(this string phrase)
+        => phrase.ReplaceWith(@"\p{Cc}", "CTRL");
+
+
+    private static string RemoveGreek(this string phrase)
+        => phrase.ReplaceWith(@"[\u03AC-\u03CE]", "");
+
+
+    private static string RemoveWhiteSpace(this string phrase)
+        => phrase.ReplaceWith(@"\s", "_");
+
+
+    private static string kebabToCamel(this string phrase)
+        => Regex.Replace(phrase, @"-(\p{L})", match => match.ToString().ToUpper());
+
+
+    private static string ReplaceWith(this string phrase, string pattern, string replacement)
+        => Regex.Replace(phrase, pattern, replacement);
 }
